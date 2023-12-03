03a839e8515c36164e0156762c746897dd887a04
Your Name <you@example.com>
2023-12-03 18:14:10 +0200

    created an arrayList for uncategories strings

--- BucketList/BucketList.java
+++ BucketList/BucketList.java
@@ -16,8 +16,15 @@ public class BucketList {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the name of the categories file:");
         String fileName=scanner.nextLine();
+        System.out.println("Enter a comma-separated list of words:");
+        String wordsString = scanner.nextLine();//variable with the list of words entered
         try{File file=new File(fileName);
             scanner= new Scanner(file);
+            while(scanner.hasNextLine()){
+                ArrayList<String> empty=new ArrayList<>();
+                map.put(scanner.nextLine(), empty);//maps the key to an arraylist of strings
+
+            }
 
         }catch(FileNotFoundException e){
             System.out.println("File not Found Askies!!");
@@ -26,6 +33,9 @@ public class BucketList {
                 scanner.close();
             }
         }
+        String[] words = wordsString.split(", ");//splits the entered line in separate words.
+
+        ArrayList<String> uncategorised = new ArrayList<String>();//array list to store the string values without a category.
 
         
     }
